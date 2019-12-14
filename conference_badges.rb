6c4d8774fecddf8098fddad6dce2d29ecb05cd1f
Ahmed Hussein <wolktheline@yahoo.com>
2019-12-14 20:20:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def batch_badge_maker["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+batch_bagde_maker.each do |a|
+  puts "Hello my name is #{a}"
+end 
+end 
+def assign_rooms [1,2,3,4,5,6,7]
+  
+  
